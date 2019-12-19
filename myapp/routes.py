e3e5dc04c6d566f463349f8ee7785f7d3b44a318
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2019-12-19 18:50:28 +0100

    Listado ventas por regiones implementando Jinja

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,12 +1,54 @@
 from myapp import app
 from flask import render_template
+import csv
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+
+    #leer el fichero sales10.csv y transformarlo en un diccionario
+    fSales_path=app.main_dir+'/data/sales10.csv'
+    print(fSales_path)
+
+    fSales = open(fSales_path,'r')
+
+    csvreader = csv.reader(fSales, delimiter=',')
+    registros = []
+    for linea in csvreader:
+        registros.append(linea)
+
+    cabecera = registros[0]
+
+    ventas = []
+    for datos in registros[1:]:
+        d = {}
+        for ix, nombre_campo in enumerate(cabecera): 
+            d[nombre_campo] = datos[ix]
+        ventas.append(d)
+    '''
+    procesarlo para obtener los totales
+    '''
+    datos = {}
+    for linea in ventas:
+        if linea['region'] in datos:
+            regAct = datos[linea['region']]
+            regAct['ingresos_totales'] += float(linea['ingresos_totales'])
+            regAct['beneficios_totales'] += float(linea['beneficio'])
+        else:
+            datos[linea['region']] = {'ingresos_totales': float(linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
+    '''
+    Finalmente devolvemos una lista de tuplas con la estructura
+    [('Region', {'ingresos_totales': valor, 'beneficios_totales': valor}),...]
+    '''
+    resultado = []
+    for clave in datos:
+        resultado.append((clave, datos[clave]))
+
+
+    '''
+    enviarlo a index.html
+    '''
+    return render_template('index.html', registros=resultado)
 
 @app.route("/detail")
 def detail():
     return render_template('detail.html')
-
-    
--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+import os
 from myapp import app
+
+app.main_dir = os.path.dirname(__file__)
 
 if __name__=='__main__':
     app.run()
